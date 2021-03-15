e820ee5bee55fa8b718f9e0e1a62be0eb406ffa7
kenfunk <kenfunkforyou@gmail.com>
2021-03-15 02:40:22 -0700

    modify historyView style

--- Draft/src/screens/HistoryScreen.js
+++ Draft/src/screens/HistoryScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, Component } from "react";
-import { Text, StyleSheet, View, FlatList, TouchableOpacity, SafeAreaView, SectionList, StatusBar } from "react-native";
+import { Text, Image, StyleSheet, View, FlatList, TouchableOpacity, SafeAreaView, SectionList, StatusBar } from "react-native";
 import firebaseUtil from '../firebase/FirebaseUtil.js'
 
 const styles = StyleSheet.create({
@@ -7,22 +7,59 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingTop: StatusBar.currentHeight,
         marginHorizontal: 50,
+        width:300,
+        marginTop:100
     },
-    row: {
+    row_odd: {
         height:40,
         display:"flex",
         flexDirection:"row",
+        justifyContent:"space-evenly",
+        alignItems:"center",
+        paddingLeft: 10,
+        backgroundColor: "white",
+        borderBottomWidth:1,
+        borderBottomColor:"white"
+    },
+    row_even: {
+        height:40,
+        display:"flex",
+        flexDirection:"row",
+        justifyContent:"space-evenly",
         alignItems:"center",
         paddingLeft: 10,
         backgroundColor: "#47C1C1",
         borderBottomWidth:1,
         borderBottomColor:"white"
     },
-    item: {
-     
+    item_pic: {
+        width:70,
+        margin:0,
+        padding:0
+    },
+    item_score: {
+        width:80,
+        margin:0,
+        padding:0
+    },
+    item_star: {
+        width:150,
+        margin:0,
+        padding:0
+    },
+    image_star: {
+        width:20,
+        height:20,
+        resizeMode: 'contain'
+    },
+    image_camera: {
+        width:30,
+        height:30,
+        resizeMode: 'contain'
     },
     header: {
-        borderRadius: 10,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
         height:50,
         display:"flex",
         justifyContent:"center",
@@ -31,121 +68,128 @@ const styles = StyleSheet.create({
         marginTop:10
     },
     header_text:{
-        color:"#d8eb34",
+        color:"#238BDB",
         fontSize: 25,
     },
+    title_container:{
+        display:"flex",
+        justifyContent:"center",
+        alignItems:"center",
+        marginBottom:15
+    },
     title: {
-      fontSize: 24
+      fontSize: 24,
+      color:"#5297DF"
     }
   });
 
 const DATA = [
     {
-        date:"Mar 03, 2021",
+        date:"March 03, 2021",
         data: [
             {
                 image_url: "ken",
                 score: 100.0,
-                star: 4,
+                star: ["star","star","star"],
                 reason: "Reason"
             },
             {
                 image_url: "Amanda",
                 score: 80.5,
-                star: 3,
+                star: ["star","star"],
                 reason: "Reason2"
             },
             {
                 image_url: "Lakshya",
                 score: 60.3,
-                star: 2,
+                star: ["star"],
                 reason: "Reason3"
             },
             {
                 image_url: "Brian",
                 score: 59.9,
-                star: 1,
+                star: ["star","star","star"],
                 reason: "Reason4"
             }
         ]
     },
     {
-        date:"Mar-04-2021",
+        date:"March 04, 2021",
         data: [
             {
                 image_url: "ken",
                 score: 100.0,
-                star: 4,
+                star: ["star"],
                 reason: "Reason"
             },
             {
                 image_url: "Amanda",
                 score: 80.5,
-                star: 3,
+                star: ["star","star"],
                 reason: "Reason2"
             },
             {
                 image_url: "Lakshya",
                 score: 60.3,
-                star: 2,
+                star: ["star","star","star"],
                 reason: "Reason3"
             },
             {
                 image_url: "Brian",
                 score: 59.9,
-                star: 1,
+                star: ["star","star","star","star"],
                 reason: "Reason4"
             }
         ]
     },
     {
-        date:"Mar-05-2021",
+        date:"March 05, 2021",
         data: [
             {
                 image_url: "ken",
                 score: 100.0,
-                star: 4,
+                star: ["star","star","star"],
                 reason: "Reason"
             },
             {
                 image_url: "Amanda",
                 score: 80.5,
-                star: 3,
+                star: ["star","star","star"],
                 reason: "Reason2"
             },
             {
                 image_url: "Lakshya",
                 score: 60.3,
-                star: 2,
+                star: ["star","star","star"],
                 reason: "Reason3"
             },
             {
                 image_url: "Brian",
                 score: 59.9,
-                star: 1,
+                star: ["star","star","star"],
                 reason: "Reason4"
             }
         ]
     },
     {
-        date:"Mar-06-2021",
+        date:"March 06, 2021",
         data: [
             {
                 image_url: "ken",
                 score: 100.0,
-                star: 4,
+                star: ["star","star","star"],
                 reason: "Reason"
             },
             {
                 image_url: "Amanda",
                 score: 80.5,
-                star: 3,
+                star: ["star","star","star"],
                 reason: "Reason2"
             },
             {
                 image_url: "Lakshya",
                 score: 60.3,
-                star: 2,
+                star: ["star","star","star"],
                 reason: "Reason3"
             }
         ]
@@ -153,10 +197,23 @@ const DATA = [
 ];
 
 const Item = ({ detail, index }) => (
-    <View style={styles.row}>
-      <Text style={styles.title}>{detail.image_url}</Text>
-      <Text style={styles.title}>{detail.score}</Text>
-      <Text style={styles.title}>{detail.star}</Text>
+    <View style={index%2 === 0? styles.row_odd: styles.row_even}>
+      <Text style={styles.item_pic}>
+          <Image style={styles.image_camera}
+                 source={require('../images/camera.png')}
+          />
+      </Text>
+      <Text style={styles.item_score}>{detail.score}</Text>
+      <Text style={styles.item_star}>
+            {
+                detail.star.map(() => {
+                        return(<Image style={styles.image_star}
+                            source={require('../images/star_filled.png')}
+                        />);
+                    }
+                )
+            }
+        </Text>
     </View>
 );
 
@@ -164,9 +221,26 @@ const Item = ({ detail, index }) => (
 // So make sure the name in your object is named "data"
 function HistoryView(props){
 
+    const [data, setData] = useState({});
+
+    // useEffect(() => {
+    //     (async () => {
+    //         const userID = 'Ken';
+    //         firebaseUtil.getJsonData(setData.bind(this), userID);
+    //         alert(data);
+    //     })();
+    // }, []);
+
     return (
         <SafeAreaView style={styles.container}>
+            <View style={styles.title_container}>
+                <Text style={styles.title}>
+                    Your Scan Records
+                </Text>
+            </View>
             <SectionList
+                showsVerticalScrollIndicator={false}
+                showsHorizontalScrollIndicator={false}
                 sections={DATA}
                 keyExtractor={(item, index) => item + index}
                 renderItem={
