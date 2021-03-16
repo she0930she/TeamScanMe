50e5f8e48a36cb4fa920ea67226e9685596fa674
Brian <bz@BriandeMacBook-Air.local>
2021-03-15 17:14:58 -0700

    Fix CSS format problem

--- Draft/src/screens/ScoreScreen.js
+++ Draft/src/screens/ScoreScreen.js
@@ -4,18 +4,15 @@ import ModalWhy from "./ModalWhy"
 
 
 const ScoreScreen = ({ navigation }) => {
-  const score = '50';
+  const score = '80';
 
   return (
 
     <View style={styles.container}>
-      <Text style={styles.textStyle}>Your Score is</Text>
+      <Text style={styles.headtextStyle}>Your Score is</Text>
       <Text style={styles.scoreStyle}>{score}</Text>
-      <Text style={styles.textStyle}>out of 100</Text>
+      <Text style={styles.bottomtextStyle}>out of 100</Text>
       <ModalWhy />
-      {/* <TouchableOpacity>
-        <Text style={styles.buttonStyle}>Why?</Text>
-      </TouchableOpacity> */}
     </View>
   );
 }
@@ -23,22 +20,23 @@ const ScoreScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 50,
+    paddingBottom: 100,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: "#FAF4C8"
   },
-  textStyle: {
+  headtextStyle: {
+    paddingTop: 130,
+    fontSize: 20
+  },
+  bottomtextStyle: {
     fontSize: 20
   },
   scoreStyle: {
     fontSize: 100,
-    color: '#F53845'
+    color: '#26FA02'
   },
-  buttonStyle: {
-    fontSize: 30,
-    color: '#352AFA'
-  }
-
 
 });
 
