27c7c9f8542b28837da658b121b00594a44362e2
Lakshya Kumar <kumar.lakshya24@gmail.com>
2021-03-21 17:20:14 -0700

    03/21: Camera screen modified to include only the click button and score alert. Working on the navigation to score screen

--- Draft/src/screens/CamaraScreen.js
+++ Draft/src/screens/CamaraScreen.js
@@ -1,164 +1,166 @@
-import React, { useState, useRef, useEffect } from "react";
-import {
-  StyleSheet,
-  Dimensions,
-  View,
-  Text,
-  TouchableOpacity,
-  SafeAreaView,
-} from "react-native";
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, Dimensions, View, TouchableOpacity, Alert, Pressable, SafeAreaView} from 'react-native';
+import firebaseUtil from '../firebase/FirebaseUtil';
 import { Camera } from "expo-camera";
 
 const WINDOW_HEIGHT = Dimensions.get("window").height;
-const closeButtonSize = Math.floor(WINDOW_HEIGHT * 0.032);
 const captureSize = Math.floor(WINDOW_HEIGHT * 0.09);
 
 export default function CamaraScreen() {
   const [hasPermission, setHasPermission] = useState(null);
   const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
-  const [isPreview, setIsPreview] = useState(false);
+  const [image, setImage] = useState();
   const [isCameraReady, setIsCameraReady] = useState(false);
   const cameraRef = useRef();
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === "granted");
-    })();
-  }, []);
-
-  const onCameraReady = () => {
-    setIsCameraReady(true);
-  };
-
-  const takePicture = async () => {
-    if (cameraRef.current) {
-      const options = { quality: 0.5, base64: true, skipProcessing: true };
-      const data = await cameraRef.current.takePictureAsync(options);
-      const source = data.uri;
-      if (source) {
-        await cameraRef.current.pausePreview();
-        setIsPreview(true);
-        alert("picture source:" + source);
-      }
-    }
-  };
-
-  const cancelPreview = async () => {
-    await cameraRef.current.resumePreview();
-    setIsPreview(false);
-  };
+    useEffect(() => {
+      (async () => {
+        const { status } = await Camera.requestPermissionsAsync();
+        setHasPermission(status === "granted");
+      })();
+    }, []);
   
-  const renderCancelPreviewButton = () => (
-    <TouchableOpacity onPress={cancelPreview} style={styles.closeButton}>
-      <View style={[styles.closeCross, { transform: [{ rotate: "45deg" }] }]} />
-      <View
-        style={[styles.closeCross, { transform: [{ rotate: "-45deg" }] }]}
-      />
-    </TouchableOpacity>
-  );
+    const onCameraReady = () => {
+      setIsCameraReady(true);
+    };
 
-  const renderCaptureControl = () => (
-    <View style={styles.control}>
-      <TouchableOpacity
-        activeOpacity={0.7}
-        disabled={!isCameraReady}
-        onPress={takePicture}
-        style={styles.capture}
-      />
-    </View>
-  );
+    const takePicture = async () => {
+      if (cameraRef.current) {
+        const options = { quality: 0.5, base64: true, skipProcessing: true };
+        const data = await cameraRef.current.takePictureAsync(options);
+        const source = data.uri;
+        if (!source.cancelled) {
+          uploadImage(source)
+          redirectScore();
+        }
+      }
+    };
 
-  if (hasPermission === null) {
-    return <View />;
-  }
-  if (hasPermission === false) {
-    return <Text style={styles.text}>No access to camera</Text>;
-  }
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <Camera
-        ref={cameraRef}
-        style={styles.container}
-        type={cameraType}
-        flashMode={Camera.Constants.FlashMode.off}
-        onCameraReady={onCameraReady}
-        onMountError={(error) => {
-          console.log("cammera error", error);
-        }}
-      />
-      <View style={styles.container}>
-        {isPreview && renderCancelPreviewButton()}
-        {!isPreview && renderCaptureControl()}
+    const redirectScore = () => {
+      <View style={styles.control}>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={Alert.alert(
+            "Image Processed",
+            "Scan results are ready to view!",
+            [
+              { 
+                text: "View Results", 
+                onPress: () => console.log("Results viewed") 
+              },
+              {
+                text: "Cancel",
+                onPress: () => takePicture,
+                style: "cancel"
+              },
+            ],
+            { cancelable: true }
+          )}
+          style={styles.capture}
+        />
       </View>
-    </SafeAreaView>
-  );
-}
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-  },
-  closeButton: {
-    position: "absolute",
-    top: 35,
-    left: 15,
-    height: closeButtonSize,
-    width: closeButtonSize,
-    borderRadius: Math.floor(closeButtonSize / 2),
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#c4c5c4",
-    opacity: 0.7,
-    zIndex: 2,
-  },
-  media: {
-    ...StyleSheet.absoluteFillObject,
-  },
-  closeCross: {
-    width: "68%",
-    height: 1,
-    backgroundColor: "black",
-  },
-  control: {
-    position: "absolute",
-    flexDirection: "row",
-    bottom: 38,
-    width: "100%",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  capture: {
-    backgroundColor: "#f5f6f5",
-    borderRadius: 5,
-    height: captureSize,
-    width: captureSize,
-    borderRadius: Math.floor(captureSize / 2),
-    marginHorizontal: 31,
-  },
-  recordIndicatorContainer: {
-    flexDirection: "row",
-    position: "absolute",
-    top: 25,
-    alignSelf: "center",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "transparent",
-    opacity: 0.7,
-  },
-  recordTitle: {
-    fontSize: 14,
-    color: "#ffffff",
-    textAlign: "center",
-  },
-  recordDot: {
-    borderRadius: 3,
-    height: 6,
-    width: 6,
-    backgroundColor: "#ff0000",
-    marginHorizontal: 5,
-  },
-  text: {
-    color: "#fff",
-  },
-});
+    };
+
+    const renderCaptureControl = () => (
+      <View style={styles.control}>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          disabled={!isCameraReady}
+          onPress={takePicture}
+          style={styles.capture}
+        />
+      </View>
+    );
+
+    const uploadImage = async(uri) => {
+        const fileName = uri.split('/').pop();
+        const response = await fetch(uri);
+        const blob = await response.blob();
+        firebaseUtil.uploadImage(setImage.bind(this), fileName, blob);
+    }
+
+    if (hasPermission === null) {
+      return <View />;
+    }
+    if (hasPermission === false) {
+      return <Text style={styles.text}>No access to camera</Text>;
+    }
+  
+    return (
+      <SafeAreaView style={styles.container}>
+        <Camera
+          ref={cameraRef}
+          style={styles.container}
+          type={cameraType}
+          flashMode={Camera.Constants.FlashMode.off}
+          onCameraReady={onCameraReady}
+          onMountError={(error) => {
+            console.log("Camera error", error);
+          }}
+        />
+        <View style={styles.container}>
+        {renderCaptureControl()}
+        </View>
+      </SafeAreaView>
+    );
+  }
+  const styles = StyleSheet.create({
+    container: {
+      ...StyleSheet.absoluteFillObject,
+    },
+    media: {
+      ...StyleSheet.absoluteFillObject,
+    },
+    control: {
+      position: "absolute",
+      flexDirection: "row",
+      bottom: 38,
+      width: "100%",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    capture: {
+      backgroundColor: "#f5f6f5",
+      borderRadius: 5,
+      height: captureSize,
+      width: captureSize,
+      borderRadius: Math.floor(captureSize / 2),
+      marginHorizontal: 31,
+    },
+    recordIndicatorContainer: {
+      flexDirection: "row",
+      position: "absolute",
+      top: 25,
+      alignSelf: "center",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "transparent",
+      opacity: 0.7,
+    },
+    recordTitle: {
+      fontSize: 14,
+      color: "#ffffff",
+      textAlign: "center",
+    },
+    text: {
+      color: "#fff",
+    }, 
+    button: {
+      borderRadius: 20,
+      padding: 10,
+    },
+    buttonOpen: {
+        backgroundColor: "#F194FF",
+    },
+    textStyle: {
+        fontSize: 30,
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center"
+    },
+    closeStyle: {
+      fontSize: 20,
+      textAlign: "center",
+      color: "white"
+  }
+  });
